a56721fc171ff49dc5c7f941be95ce5baf117a64
ozod11052008 <ozodabduvoitov8@gmail.com>
2022-08-09 11:28:30 +0300

    second

--- config/config.js
+++ config/config.js
@@ -3,8 +3,6 @@ const mongoose = require('mongoose')
 const connectDB = async () => {
     await mongoose.connect('mongodb+srv://yosh_dasturchi:11052008ozod@cluster0.qih2a9m.mongodb.net/?retryWrites=true&w=majority', {
         useNewUrlParser : true,
-        useUnifiedTopology : true,
-        family : 4
     })
     .then(data => {
         console.log('Mongodb is connected');
