599de458337c7ce82565987f9a70f39dd36696d8
ozod11052008 <ozodabduvoitov8@gmail.com>
2022-08-09 11:41:07 +0300

    second

--- config/config.js
+++ config/config.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 
 const connectDB = async () => {
-    await mongoose.connect('mongodb+srv://yosh_dasturchi:11052008ozod@cluster0.qih2a9m.mongodb.net/?retryWrites=true&w=majority', {
+    await mongoose.connect('mongodb+srv://Shohruh:349672@cluster0.pelmz.mongodb.net/price?retryWrites=true&w=majority', {
         useNewUrlParser : true, 
         useUnifiedTopology: true, 
         serverApi: ServerApiVersion.v1 
